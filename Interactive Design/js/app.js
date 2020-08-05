0c264df591c90d8a712fad85c561cb27fb888d5e
cperry21 <65249358+cperry21@users.noreply.github.com>
2020-08-04 23:45:53 -0500

    added js to jumpsite

---
+++ Interactive Design/js/app.js
@@ -0,0 +1,14 @@
+/* app.js */
+
+// create a variable motd and assign a string to it
+let motdArray = [
+
+"Unless someone like you cares a whole awful lot, nothing is going to get better. It’s not. <i>--Dr Seuss</i>"
+    
+];
+
+
+
+//query the document and get an element with an id of 'motd'
+// and assign our character string to it.
+documnet.querySelector("#motd").innerHTML =motd;
